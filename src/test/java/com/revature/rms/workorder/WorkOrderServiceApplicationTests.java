1702ee1fc356d9756bf545924d1bf2e2d8d31276
wsingleton <wezley.singleton@gmail.com>
2020-03-03 00:07:35 -0500

    init repo and project

---
+++ src/test/java/com/revature/rms/workorder/WorkOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.rms.workorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
